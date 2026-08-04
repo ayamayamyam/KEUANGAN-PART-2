ab4a92f72993a142a082668c4b9d2e2de295048a
ayamayamyam <pitik2109@gmail.com>
2026-08-04 10:28:32 +0700

    Update ReminderRepository.kt

--- app/src/main/java/com/student/finance/data/repository/ReminderRepository.kt
+++ app/src/main/java/com/student/finance/data/repository/ReminderRepository.kt
@@ -8,8 +8,8 @@ import javax.inject.Singleton
 
 @Singleton
 class ReminderRepository @Inject constructor(private val dao: ReminderDao) {
-    fun getAll(): Flow<List<ReminderEntity>> = dao.getAll()
-    fun getEnabled(): Flow<List<ReminderEntity>> = dao.getEnabled()
+    fun getAll(accountId: Long): Flow<List<ReminderEntity>> = dao.getAll(accountId)
+    fun getEnabled(accountId: Long): Flow<List<ReminderEntity>> = dao.getEnabled(accountId)
     suspend fun insert(reminder: ReminderEntity) = dao.insert(reminder)
     suspend fun update(reminder: ReminderEntity) = dao.update(reminder)
     suspend fun delete(reminder: ReminderEntity) = dao.delete(reminder)
